99e19380ad48e8879bac93b961e67aa71f54f620
Pravin Tripathi <pravinyo12@gmail.com>
2021-05-03 23:26:45 +0530

    retry sync added

--- processor/src/main/java/com/example/processor/ProcessorConfig.java
+++ processor/src/main/java/com/example/processor/ProcessorConfig.java
@@ -1,5 +1,6 @@
 package com.example.processor;
 
+import java.time.Duration;
 import java.util.Properties;
 
 import com.linecorp.decaton.processor.runtime.*;
@@ -22,17 +23,18 @@ public class ProcessorConfig {
     private static final String SUBSCRIPTION_ID = "hello-processor";
     private static final String TOPIC = "topic1";
 
+//    @Bean
+//    public ProcessorSubscription helloProcessorSubscription(HelloTaskProcessor processor) {
+//        return newProcessorSubscription(SUBSCRIPTION_ID, TOPIC, HelloTask.parser(), processor);
+//    }
+
     @Bean
-    public ProcessorSubscription helloProcessorSubscription(HelloTaskProcessor processor) {
-        return newProcessorSubscription(SUBSCRIPTION_ID, TOPIC, HelloTask.parser(), processor);
+    public ProcessorSubscription retryProcessorSubscription(RetryTaskProcessorSync processor) {
+        return newProcessorSubscriptionWithRetry(SUBSCRIPTION_ID, TOPIC, HelloTask.parser(), processor);
     }
 
     private static <T extends GeneratedMessageV3> ProcessorSubscription newProcessorSubscription(
             String subscriptionId, String topic, Parser<T> parser, DecatonProcessor<T> processor) {
-        Properties config = new Properties();
-        config.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, CLIENT_ID);
-        config.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-        config.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
 
         ProcessorsBuilder<T> processorsBuilder =
                 ProcessorsBuilder.consuming(topic, new ProtocolBuffersDeserializer<>(parser))
@@ -47,8 +49,42 @@ public class ProcessorConfig {
 
         return SubscriptionBuilder.newBuilder(subscriptionId)
                                   .processorsBuilder(processorsBuilder)
-                                  .consumerConfig(config)
+                                  .consumerConfig(propertyConfig())
                                   .properties(propertySupplier)
                                   .buildAndStart();
+    }
+
+    private static <T extends GeneratedMessageV3> ProcessorSubscription newProcessorSubscriptionWithRetry(
+            String subscriptionId, String topic, Parser<T> parser, DecatonProcessor<T> processor) {
+
+
+        /* Decaton needs a retry topic for retry functionality. The name of the retry topic will be
+           `topic name` + `-retry` (topic1-retry) by default. For example, if you have a topic called weather-task then
+           you need to create a topic called weather-task-retry for retry function. You can also
+           overwrite the default naming convention.
+         */
+        ProcessorsBuilder<T> processorsBuilder =
+                ProcessorsBuilder.consuming(topic, new ProtocolBuffersDeserializer<>(parser))
+                        .thenProcess(processor);
+
+        PropertySupplier propertySupplier =
+                StaticPropertySupplier.of(
+                        Property.ofStatic(ProcessorProperties.CONFIG_PARTITION_CONCURRENCY, 10),
+                        Property.ofStatic(ProcessorProperties.CONFIG_MAX_PENDING_RECORDS, 100));
+
+        return SubscriptionBuilder.newBuilder(subscriptionId)
+                .enableRetry(RetryConfig.withBackoff(Duration.ofMillis(100)))
+                .processorsBuilder(processorsBuilder)
+                .consumerConfig(propertyConfig())
+                .properties(propertySupplier)
+                .buildAndStart();
+    }
+
+    private static Properties propertyConfig(){
+        Properties config = new Properties();
+        config.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, CLIENT_ID);
+        config.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        config.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        return config;
     }
 }
---
+++ processor/src/main/java/com/example/processor/RetryTaskProcessorSync.java
@@ -0,0 +1,47 @@
+package com.example.processor;
+
+import com.example.protocol.Tasks;
+import com.google.protobuf.Timestamp;
+import com.linecorp.decaton.processor.DecatonProcessor;
+import com.linecorp.decaton.processor.ProcessingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
+@Component
+public class RetryTaskProcessorSync implements DecatonProcessor<Tasks.HelloTask> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RetryTaskProcessorSync.class);
+
+    @Override
+    public void process(ProcessingContext<Tasks.HelloTask> context, Tasks.HelloTask task) throws InterruptedException {
+
+        try {
+            Timestamp timestamp = task.getCreatedAt();
+            LocalDateTime dateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp.getSeconds(),
+                    timestamp.getNanos()),
+                    TimeZone.getDefault().toZoneId());
+            if (timestamp.getSeconds() % 2 == 0){
+                // to simulate 3rd party system is slow
+                throw new IOException("Source system is not responding");
+            }
+            LOGGER.info("message={} createdAt={}", task.getMessage(), dateTime);
+        }catch (Exception ex){
+            LOGGER.info("Task cannot be completed with error {}",ex.getMessage());
+
+            if (context.metadata().retryCount() > 5){
+                /* Decaton will record how many time a task has been retried. You can use this method
+                   to get the number of counts.
+                 */
+                LOGGER.error("Task retied for {} times but action cannot be completed",context.metadata().retryCount());
+            }else {
+                context.retry(); // Decaton will handle remaining works for you.
+                LOGGER.info("Task({}) is scheduled for retry",task.getMessage());
+            }
+        }
+    }
+}
