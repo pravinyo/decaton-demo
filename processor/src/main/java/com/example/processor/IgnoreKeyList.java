0aadc5946afaa89c6dd32de7c7427daa2284005d
Pravin Tripathi <pravinyo12@gmail.com>
2021-05-04 10:31:49 +0530

    comments added

--- processor/src/main/java/com/example/processor/IgnoreKeyList.java
+++ processor/src/main/java/com/example/processor/IgnoreKeyList.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class IgnoreKeyList {
+    // Decaton supports defining a list of keys that you want to skip when processing task.
+    // The list can be dynamic. It means that you can add or remove keys from the list on the fly.
     static List<String> getKeysToIgnore(){
         List<String> keys = new ArrayList<>();
         keys.add("10");
